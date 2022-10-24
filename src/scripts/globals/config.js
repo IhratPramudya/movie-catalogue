87e49324d918fd1f393e4287f17cf14160229558
Ihrat Pramudya <90922429+IhratPramudya@users.noreply.github.com>
2022-10-24 10:15:40 +0700

    Update config.js
    Menambahkan Api Key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: 'f6a7d271c7794132e7189c1a387f1423',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
